e418651594965e167796a3bb7b3a58cabbdd13b9
Sanjar0126 <sanjar.niyozov.0126@gmail.com>
2021-06-28 16:43:41 +0500

    push

--- main.go
+++ main.go
@@ -2,22 +2,66 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"log"
+	"net/http"
 	"os"
-	"time"
 )
 
-func main() {
-	conn, err := pgxpool.Connect(context.Background(), "postgres://iman_user:iman_root@localhost:5432/golangdb")
+const DbUrl = "postgres://iman_user:iman_root@localhost:5432/golangdb"
+
+func connectDB() *pgxpool.Pool {
+	pool, err := pgxpool.Connect(context.Background(), DbUrl)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
+	}else{
+		fmt.Println("Connected to database")
 	}
-	defer conn.Close()
+	return pool
+}
 
-	contact := ContactList{"Sanjar", "dot@mail.ty",
-						"Main Street", 989341233, time.Now()}
+func hello(w http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(w, "Hello world\n")
+	body := json.NewDecoder(req.Body)
+	log.Print(body)
+}
 
-	contact.CreateContactList(conn)
+func main() {
+	dbConnection := connectDB()
+	defer dbConnection.Close()
+
+	http.HandleFunc("/", hello)
+
+	err := http.ListenAndServe(":8099", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//contact := ContactList{}
+
+	//contact.CreateContactList(dbConnection)
+
+	//contact.GetContact(dbConnection, 4)
+	//fmt.Printf("ID\tName\tE-Mail\t\tAddress\t\tPhone Number\tCreated Date\n")
+	//fmt.Printf("%d\t%s\t%s\t%s\t%d\t%s\t%s\n",
+	//	contact.id, contact.name, contact.email, contact.address, contact.phoneNumber, contact.createdAt.String(), contact.updatedAt.String())
+
+	//contacts := contact.GetContactList(dbConnection)
+	//defer contacts.Close()
+	//fmt.Printf("ID\tName\tE-Mail\t\tAddress\t\tPhone Number\tCreated Date\n")
+	//for contacts.Next(){
+	//	err := contacts.Scan(&contact.id, &contact.name, &contact.email, &contact.address, &contact.phoneNumber, &contact.createdAt, &contact.updatedAt)
+	//	if err != nil {
+	//		return
+	//	}
+	//fmt.Printf("%d\t%s\t%s\t%s\t%d\t%s\t%s\n",
+	//	contact.id, contact.name, contact.email, contact.address, contact.phoneNumber, contact.createdAt.String(), contact.updatedAt.String())
+	//}
+
+	//contact.UpdateContact(dbConnection, 4)
+	//contact.DeleteContact(dbConnection, 5)
 }
--- models.go
+++ models.go
@@ -3,23 +3,66 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
 	"time"
 )
 
 type ContactList struct {
+	id					 int
 	name, email, address string
 	phoneNumber          int
 	createdAt            time.Time
+	updatedAt			 time.Time
+}
+
+type ContactListInterface interface {
+	CreateContactList(conn *pgxpool.Pool)
+	GetContact(conn *pgxpool.Pool, contactId int)
+	GetContactList(conn *pgxpool.Pool) pgx.Rows
+	UpdateContact(conn *pgxpool.Pool, contactId int)
+	DeleteContact(conn *pgxpool.Pool, contactId int)
 }
 
 func (c *ContactList) CreateContactList(conn *pgxpool.Pool) {
-	sqlQuery := fmt.Sprintf("insert into contactlist (name, email, address, phonenumber, createdat) VALUES('%s', '%s', '%s', '%d', '%s')", c.name, c.email, c.address, c.phoneNumber, c.createdAt)
-	_ , err := conn.Exec(context.Background(), sqlQuery)
+	_ , err := conn.Exec(context.Background(), "insert into contactlist (name, email, address, phonenumber, createdat, updated_at) VALUES($1, $2, $3, $4, $5, $6)",
+		c.name, c.email, c.address, c.phoneNumber, c.createdAt, c.updatedAt)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "Query create failed: %v\n", err)
+	}
+}
+
+func (c *ContactList) GetContact(conn *pgxpool.Pool, contactId int) {
+	err := conn.QueryRow(context.Background(), "select * from contactlist where id=$1", contactId).
+		Scan(&c.id, &c.name, &c.email, &c.address, &c.phoneNumber, &c.createdAt, &c.updatedAt)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query get failed: %v\n", err)
+	}
+}
+
+func (c *ContactList) GetContactList(conn *pgxpool.Pool) pgx.Rows {
+	rows, err := conn.Query(context.Background(), "select * from contactlist order by id")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query get list failed: %v\n", err)
+	}
+	return rows
+}
+
+func (c *ContactList) UpdateContact(conn *pgxpool.Pool, contactId int) {
+	_, err := conn.Exec(context.Background(),
+		"update contactlist set name=$2, email=$3, address=$4, phonenumber=$5, updated_at=$6 where id=$1",
+		contactId, c.name, c.email, c.address, c.phoneNumber, c.updatedAt)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query update failed: %v\n", err)
+	}
+}
+
+func (c *ContactList) DeleteContact(conn *pgxpool.Pool, contactId int) {
+	_, err := conn.Exec(context.Background(),
+		"delete from contactlist where id=$1", contactId)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query update failed: %v\n", err)
 	}
 }
 
@@ -28,4 +71,5 @@ type TaskList struct {
 	task      string
 	completed bool
 	createdAt time.Time
+	updatedAt time.Time
 }
